59108c55b85db225e54cea87a6a85bd79ba54539
janeberry <janeuarykim@gmail.com>
2025-04-30 19:38:39 +0900

    update img_filePath with absolute path

--- tangerbot/src/gui/user_gui/src/tangerine.cpp
+++ tangerbot/src/gui/user_gui/src/tangerine.cpp
@@ -11,21 +11,20 @@ Tangerine::Tangerine(QWidget *parent) :
   ui(new Ui::Tangerine)
 {
   ui->setupUi(this);
-  // QString imagePath = QDir::current().absolutePath("/src/gui/user_gui/ui/mainPic.png");
-  // //QString imagePath = QDir::fromNativeSeparators(QCoreApplication::applicationDirPath() + "/../ui/mainPic.png");
-
-  // qDebug() << "Current Directory: " << QDir::current();
-
-
-  // Debugging the current directory
+  
+  /**
+   * * Load the main image
+   */
   QString path = QDir::current().absolutePath();
   QString imagePath = path + "/src/gui/user_gui/ui/mainPic.png";
-  
-  qDebug() << "Image Path: " << imagePath;
-  // Load the image into the label
+  //qDebug() << "Image Path: " << imagePath;
   QPixmap pixmap(imagePath); 
   ui->label_introPic->setPixmap(pixmap);
   ui->label_introPic->setScaledContents(true);
+
+  /**
+   * TODO: clicked button connect (sign in and sign up)
+   */
 }
 
 
