fcd7d6eb66ddb7dc3581a9fcac3d2a1158382aba
janeberry <janeuarykim@gmail.com>
2025-05-08 21:55:42 +0900

    tried to update map

--- tangerbot/src/gui/user_gui/include/user_gui/tangerine.h
+++ tangerbot/src/gui/user_gui/include/user_gui/tangerine.h
@@ -1,6 +1,3 @@
-   
-
-
 #ifndef TANGERINE_H
 #define TANGERINE_H
 
@@ -21,37 +18,37 @@ namespace Ui {
 
 class Tangerine : public QMainWindow
 {
-  Q_OBJECT
+
+    Q_OBJECT
 
 public:
     explicit Tangerine(QWidget *parent = nullptr);
     ~Tangerine();
 
+private slots:
+    void updateMonitoringMap();
+
 private:
     Ui::Tangerine *ui;
+
+    void setupMapSubscriber();
+    void processMap(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
+    void sendGoalFromClick(const QPoint &click_pos);
+    void mousePressEvent(QMouseEvent *event) override;
+
+
+    rclcpp::Node::SharedPtr node_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pub_;
+    rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_subscriber_;
+
+    
+    bool map_received_;
+    QImage map_image_;
+    nav_msgs::msg::MapMetaData map_metadata_;
+
+    rviz_common::RenderPanel *robot_render_panel_;
+    rviz_common::VisualizationManager *robot_vis_manager_;
+    rviz_common::Display *map_display_;
 };
 
-// private:
-//   void initializeRViz();
-//   void setupMapDisplay();
-//   void setupMapSubscriber();
-//   void setupRobotModelDisplay();
-//   void setupLaserScanDisplay();
-//   void setupJoystickControls();
-//   void sendJoystickCommand();
-
-//   Ui::Tangerine *ui;
-//   rclcpp::Node::SharedPtr node_;
-//   rviz_common::RenderPanel *main_render_panel_;  // For Main page
-//   rviz_common::RenderPanel *robot_render_panel_; // For Robot page
-//   rviz_common::VisualizationManager *main_vis_manager_;
-//   rviz_common::VisualizationManager *robot_vis_manager_;
-//   rviz_common::Display *map_display_;
-//   rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_publisher_;
-//   rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr goal_pose_publisher_;
-//   rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr map_subscriber_;
-//   geometry_msgs::msg::Twist current_twist_;
-//   bool map_received_;
-// };
-
-#endif // TANGERINE_H
+#endif
