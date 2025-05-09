c4ceaf8503cadbff74cf43a23bb4615c27fd5f83
janeberry <janeuarykim@gmail.com>
2025-05-09 17:58:05 +0900

    command send voice data

--- tangerbot/src/gui/user_gui/src/tangerine.cpp
+++ tangerbot/src/gui/user_gui/src/tangerine.cpp
@@ -156,9 +156,9 @@ void Tangerine::updateMonitoringMap(){
     ui->monitoringMap->setPixmap(pixmap);
     ui->monitoringMap->setScaledContents(true);
   }
+}
 
-
-void Tangerine::sendVoiceData(){
+// void Tangerine::sendVoiceData(){
     
-}
-}
+
+// }
